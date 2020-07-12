35a38ddec5cf95e6345df47cce0460b0e65d3643
Nariyuki Saito <toshincompos@gmail.com>
2020-07-12 17:48:42 +0900

    Add cloudfront settings

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@ const exec = require('child_process').exec
 const config = require('../config.js')
 
 const main = () => {
-    exec(`sam deploy --template-file cloudformation.yaml --stack-name ${config.stackName} --s3-bucket ${config.artifactsS3BucketName} --region ${config.region} --capabilities CAPABILITY_IAM`,
+    exec(`sam deploy --template-file cloudformation.yaml --stack-name ${config.stackName} --s3-bucket ${config.artifactsS3BucketName} --region ${config.region} --capabilities CAPABILITY_IAM --parameter-overrides StaticAssetsS3BucketName="${config.staticAssetsS3BucketName}" `,
         (err, stdout, stderr) => {
             if (err) {
                 console.log(stderr)
